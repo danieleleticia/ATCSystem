e8b787c82f4359146137652986310b94d69d13b7
AntonioFSN2 <antonio.af@gec.inatel.br>
2025-11-24 01:15:59 -0300

    adicionei a classe carga

---
+++ src/br/inatel/c06/atcsystem/carga/Carga.java
@@ -0,0 +1,25 @@
+package br.inatel.c06.atcsystem.carga;
+
+public class Carga {
+    private String tipo;
+    private double peso;
+    private boolean sensivel;
+    private boolean perecivel;
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public boolean isSensivel() {
+        return sensivel;
+    }
+
+    public boolean isPerecivel() {
+        return perecivel;
+    }
+}
+
